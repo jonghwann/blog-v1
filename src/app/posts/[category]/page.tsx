9f8a3ed2228febf5efa63127431905b420de10c4
jangjonghwan <wkdwhdghks97@gmail.com>
2024-12-16 22:46:15 +0900

    shadcn/ui 설치, tabs 테스트

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -1,5 +1,7 @@
 import { getAllPosts, getCategories } from '@/lib/post';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 interface Props {
   params: Promise<{ category: string }>;
 }
@@ -12,6 +14,15 @@ export default async function Page({ params }: Props) {
 
   return (
     <div>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+
       <ul>
         {categories.map((category) => (
           <li key={category.category}>
