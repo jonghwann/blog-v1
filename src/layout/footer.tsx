57ab82aa83603a6dd2f859d8202c2b29a69dad69
jangjonghwan <wkdwhdghks97@gmail.com>
2025-01-04 19:09:55 +0900

    푸터 구현

---
+++ src/layout/footer.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import { getYear } from 'date-fns';
+
+import Github from '@/components/icons/github';
+import LinkedIn from '@/components/icons/linked-in';
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center gap-4 bg-[--color-bg] py-16">
+      <div className="flex items-center gap-4">
+        <Link href="https://github.com/wkdwhdghks" target="_blank">
+          <Github className="fill-[--color-text]" width={30} height={30} />
+        </Link>
+
+        <Link href="/">
+          <LinkedIn className="fill-[--color-text]" width={30} height={30} />
+        </Link>
+      </div>
+
+      <span>© {getYear(new Date())} Powered by Jonghwan Jang</span>
+    </footer>
+  );
+}
