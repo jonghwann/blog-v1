9281a6483f43cae078cec24dc025be450af260e0
jangjonghwan <wkdwhdghks97@gmail.com>
2025-01-09 17:02:45 +0900

    category-select 구현

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -1,6 +1,7 @@
 import { getPosts, getCategories } from '@/lib/post';
 
 import CategoryTabs from '@/components/molecules/category-tabs';
+import CategorySelect from '@/components/atoms/category-select';
 import PostCard from '@/components/molecules/post-card';
 
 interface Props {
@@ -14,7 +15,9 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="w-full">
-      <CategoryTabs className="mb-10" defaultValue={category} categories={categories} baseUrl={'/posts'} />
+      <CategoryTabs className="mb-10 hidden sm:block" defaultValue={category} categories={categories} baseUrl={'/posts'} />
+
+      <CategorySelect classNames={{ trigger: 'mb-10 sm:hidden' }} defaultValue={category} categories={categories} baseUrl={'/posts'} />
 
       <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => {
--- src/components/molecules/category-tabs.tsx
+++ src/components/molecules/category-tabs.tsx
@@ -1,30 +1,38 @@
 import Link from 'next/link';
 
+import { cn } from '@/lib/utils';
+
 import { Categories } from '@/types/post';
 
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 interface CategoryTabsProps {
   className?: string;
+  classNames?: { trigger?: string };
   defaultValue: string;
   categories: Categories[];
   baseUrl: string;
 }
 
-export default function CategoryTabs({ className, defaultValue, categories, baseUrl }: CategoryTabsProps) {
+export default function CategoryTabs({ className, classNames = {}, defaultValue, categories, baseUrl }: CategoryTabsProps) {
   return (
     <Tabs className={className} defaultValue={defaultValue}>
       <TabsList className="gap-2">
         {categories.map(({ category, count }) => {
           return (
-            <Link key={category} href={category === 'all' ? baseUrl : `${baseUrl}/${category}`}>
-              <TabsTrigger
-                value={category}
-                className="data-[state=active]:text-contrast-foreground py-2 hover:bg-secondary-background data-[state=active]:bg-contrast-background data-[state=active]:shadow-none hover:data-[state=active]:bg-[#24292f] hover:data-[state=active]:dark:bg-[#f0f3f6]"
-              >
+            <TabsTrigger
+              key={category}
+              value={category}
+              className={cn(
+                'py-2 hover:bg-secondary-background data-[state=active]:bg-contrast-background data-[state=active]:text-contrast-foreground data-[state=active]:shadow-none hover:data-[state=active]:bg-[#24292f] hover:data-[state=active]:dark:bg-[#f0f3f6]',
+                classNames.trigger,
+              )}
+              asChild
+            >
+              <Link href={category === 'all' ? baseUrl : `${baseUrl}/${category}`}>
                 {category} ({count})
-              </TabsTrigger>
-            </Link>
+              </Link>
+            </TabsTrigger>
           );
         })}
       </TabsList>
