cf9dea3b929209aba92493c77a8669e3156650a3
jangjonghwan <wkdwhdghks97@gmail.com>
2025-01-16 22:59:05 +0900

    refactor: post-card 시맨틱 태그 수정

--- src/components/molecules/post-card.tsx
+++ src/components/molecules/post-card.tsx
@@ -9,33 +9,31 @@ interface PostCardProps {
 
 export default function PostCard({ post: { thumbnail, title, category, createdAt, readingMinutes, postUrl } }: PostCardProps) {
   return (
-    <li>
-      <article className="group overflow-hidden rounded-[20px] shadow-[0_15px_30px_0_rgba(0,0,0,0.05)] will-change-transform">
-        <Link href={postUrl}>
-          <figure className="relative h-[210px] overflow-hidden">
-            <Image
-              src={thumbnail}
-              alt={title}
-              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-              fill
-              priority
-              className="object-cover duration-300 ease-in-out group-hover:scale-[1.08]"
-            />
-          </figure>
+    <article className="group overflow-hidden rounded-[20px] shadow-[0_15px_30px_0_rgba(0,0,0,0.05)] will-change-transform">
+      <Link href={postUrl}>
+        <figure className="relative h-[210px] overflow-hidden">
+          <Image
+            src={thumbnail}
+            alt={title}
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+            fill
+            priority
+            className="object-cover duration-300 ease-in-out group-hover:scale-[1.08]"
+          />
+        </figure>
 
-          <div className="flex h-[170px] flex-col justify-between bg-white p-[26px] dark:bg-[#1a1b1e]">
-            <div className="flex flex-col gap-[6px]">
-              <div className="text-xs font-medium">{category}</div>
-              <h2 className="text-xl font-bold">{title}</h2>
-            </div>
-
-            <div className="flex items-center justify-between text-xs text-secondary-foreground">
-              <time className="flex items-center gap-1">{createdAt}</time>
-              <span>{readingMinutes}분</span>
-            </div>
+        <div className="flex h-[170px] flex-col justify-between bg-white p-[26px] dark:bg-[#1a1b1e]">
+          <div className="flex flex-col gap-[6px]">
+            <div className="text-xs font-medium">{category}</div>
+            <h2 className="text-xl font-bold">{title}</h2>
           </div>
-        </Link>
-      </article>
-    </li>
+
+          <div className="flex items-center justify-between text-xs text-secondary-foreground">
+            <time className="flex items-center gap-1">{createdAt}</time>
+            <span>{readingMinutes}분</span>
+          </div>
+        </div>
+      </Link>
+    </article>
   );
 }
