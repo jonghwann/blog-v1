718b73c9b1c13c5ebf76a1f65292c1cbd0032c28
jangjonghwan <wkdwhdghks97@gmail.com>
2025-01-20 14:52:53 +0900

    feat: giscus 연동

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -5,7 +5,7 @@ import LinkedIn from '@/components/icons/linked-in';
 
 export default function Footer() {
   return (
-    <footer className="flex h-14 items-center justify-center gap-3 border-t border-border bg-background">
+    <footer className="mt-[56px] flex h-14 items-center justify-center gap-3 border-t border-border bg-background">
       <span className="text-sm">© Powered by Jonghwan Jang</span>
 
       <div className="flex items-center gap-2">
