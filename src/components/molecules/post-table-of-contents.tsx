47952eb4688ad08bc5edcd13dde8a4ec206bf637
jonghwann <jonghwan0729@gmail.com>
2025-02-11 20:36:50 +0900

    style: PostTableOfContents 폰트 크기 조정

--- src/components/molecules/post-table-of-contents.tsx
+++ src/components/molecules/post-table-of-contents.tsx
@@ -18,14 +18,13 @@ export default function PostTableOfContents({ className, tableOfContents }: Post
 
   return (
     <aside className={cn('block w-[200px] shrink-0', className)}>
-      <div className="sticky top-[128px] text-sm">
-        <h2 className="mb-2 font-medium">On this page</h2>
-
+      <div className="sticky top-[120px] text-xs">
+        <h2 className="mb-1.5 text-sm font-medium">On this page</h2>
         <nav>
-          <ul className="flex flex-col gap-2">
+          <ul className="flex flex-col gap-1.5">
             {tableOfContents.map(({ title, link, depth }) => (
-              <li key={title} className={depth === 1 ? 'ml-4' : ''}>
-                <Link href={link} className={activeIds.includes(link.slice(1)) ? 'text-accent-foreground' : 'text-secondary-foreground'}>
+              <li key={title} className={depth === 1 ? 'ml-3' : ''}>
+                <Link href={link} className={activeIds.includes(link.slice(1)) ? 'font-semibold text-accent-foreground' : 'text-secondary-foreground'}>
                   {title}
                 </Link>
               </li>
