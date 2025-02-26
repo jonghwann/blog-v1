ad347a0da043746e852b6f408bc97322deb7ad16
jangjonghwan <wkdwhdghks97@gmail.com>
2025-02-26 22:56:29 +0900

    fix: system theme일 경우 다크/라이트 모드 바뀌지 않는 오류 수정

--- src/components/atoms/theme-toggle-button.tsx
+++ src/components/atoms/theme-toggle-button.tsx
@@ -1,23 +1,46 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 import { useTheme } from 'next-themes';
-import { Sun, Moon } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Dot, Monitor, Moon, Sun } from 'lucide-react';
 
 export default function ThemeToggleButton() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
   return (
-    <button className="icon-box" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-      {theme === 'light' ? <Moon size={20} /> : <Sun size={20} />}
-    </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="icon-box shadow-none focus-visible:outline-none focus-visible:ring-0">
+          <Sun className="block h-5 w-5 dark:hidden" />
+          <Moon className="hidden h-5 w-5 dark:block" />
+          <span className="sr-only">Toggle theme</span>
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-background" align="end">
+        {themeOptions.map(({ mode, icon }) => (
+          <DropdownMenuItem className="cursor-pointer leading-normal" key={mode} onClick={() => setTheme(mode)}>
+            {icon}
+            {mode}
+            {theme === mode && <Dot className="absolute right-2" />}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
+
+const themeOptions = [
+  {
+    mode: 'light',
+    icon: <Sun />,
+  },
+  {
+    mode: 'dark',
+    icon: <Moon />,
+  },
+  {
+    mode: 'system',
+    icon: <Monitor />,
+  },
+];
