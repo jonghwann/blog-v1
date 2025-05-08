91d63fa578a801f7155d2d79ae18c26073bb3e85
jangjonghwan <wkdwhdghks97@gmail.com>
2025-05-08 23:28:40 +0900

    style: 코드 블록 스타일 @apply로 정리

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
           <p className="text-lg">찾을 수 없는 페이지입니다.</p>
         </div>
 
-        <Button className="h-10 bg-secondary-background px-10 text-secondary-foreground shadow-none transition-none hover:bg-tertiary-background" asChild>
+        <Button className="bg-secondary text-secondary-foreground hover:bg-muted h-10 px-10 shadow-none transition-none" asChild>
           <Link href="/posts">홈으로</Link>
         </Button>
       </div>
