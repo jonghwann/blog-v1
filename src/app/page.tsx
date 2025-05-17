eba4c91ebfcc749960f201610a7b0aae49794213
jonghwann <jonghwan0729@gmail.com>
2025-05-17 15:44:21 +0900

    chore: 프로젝트 초기화 및 초기 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation';
-
-export default function Page() {
-  redirect('/posts');
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
--- src/app/posts/page.tsx
+++
@@ -1,5 +0,0 @@
-import CategoryPage from './[category]/page';
-
-export default async function PostsPage() {
-  return <CategoryPage params={Promise.resolve({ category: 'all' })} />;
-}
