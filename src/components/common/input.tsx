d0de9f2495b9761b615ea4273b7c276945896e46
jonghwann <jonghwan0729@gmail.com>
2025-05-26 21:04:02 +0900

    feat: Input 컴포넌트 구현

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,15 +1,29 @@
 import { InputHTMLAttributes } from 'react';
 
+import { cn } from '@/lib/utils';
+
+import { Input as InputUI } from '@/components/ui/input';
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  errorClassName?: string;
   name: string;
   errors?: string[];
 }
 
-export default function Input({ name, errors, ...rest }: InputProps) {
+export default function Input({ className, errorClassName, name, errors, ...rest }: InputProps) {
   return (
-    <div className="flex flex-col">
-      <input name={name} {...rest} className="rounded border p-2" />
-      {errors?.[0] && <span className="font-medium text-red-500">{errors[0]}</span>}
+    <div className="flex flex-col gap-2">
+      <InputUI
+        className={cn(
+          'autofill:bg-background h-12 rounded-lg py-0 text-base! shadow-none focus-visible:ring-2',
+          className,
+        )}
+        name={name}
+        {...rest}
+      />
+
+      {errors?.[0] && <span className={cn('text-error text-sm', errorClassName)}>{errors[0]}</span>}
     </div>
   );
 }
