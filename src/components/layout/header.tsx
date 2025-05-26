3aaf86c7cb3cef55004c7d6bee7b8cb65de720f7
jangjonghwan <wkdwhdghks97@gmail.com>
2025-05-26 21:19:02 +0900

    feat: 로그아웃 기능 구현

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,44 +1,9 @@
-'use client';
+import getSession from '@/lib/session';
 
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import ClientHeader from './client-header';
 
-import useScrollVisibility from '@/hooks/use-scroll-visibility';
+export default async function Header() {
+  const { id } = await getSession();
 
-import { cn } from '@/lib/utils';
-
-const NAV_LINKS = [
-  { name: 'Posts', href: '/posts' },
-  { name: 'About', href: '/about' },
-];
-
-export default function Header() {
-  const pathname = usePathname();
-
-  const marginTop = useScrollVisibility(64);
-
-  return (
-    <header
-      className="bg-background/80 fixed top-0 w-full border-b backdrop-blur-[5px] backdrop-saturate-[180%]"
-      style={{ marginTop }}
-    >
-      <nav className="mx-auto flex h-16 w-full max-w-(--breakpoint-xl) items-center px-4">
-        <ul className="flex items-center gap-6">
-          {NAV_LINKS.map(({ name, href }) => (
-            <li key={href}>
-              <Link
-                className={cn(
-                  'text-secondary-foreground hover:text-foreground text-sm transition-colors duration-150 ease-in-out',
-                  pathname.startsWith(href) && 'text-accent-foreground hover:text-accent-foreground',
-                )}
-                href={href}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+  return <ClientHeader id={id} />;
 }
