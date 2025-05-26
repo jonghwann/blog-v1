bea9fb6e6458199f974a8acdd397352bc1ae2502
jangjonghwan <wkdwhdghks97@gmail.com>
2025-05-26 21:29:11 +0900

    feat: 인증 기반 라우트 접근 제어 미들웨어 구현

--- src/app/logout/action.ts
+++ src/app/logout/action.ts
@@ -1,8 +1,11 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+
 import getSession from '@/lib/session';
 
 export async function logoutAction() {
   const session = await getSession();
   session.destroy();
+  redirect('/posts');
 }
