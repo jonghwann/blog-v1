253cf57c7dc4d6e1c2e0cc66db1fd1cee6620989
jangjonghwan <wkdwhdghks97@gmail.com>
2025-05-30 22:13:23 +0900

    refactor: Header 컴포넌트 로직 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,26 @@
+import Form from 'next/form';
+
 import getSession from '@/lib/session';
 
-import ClientHeader from './client-header';
+import { logoutAction } from '@/app/logout/action';
+
+import Nav from './nav';
+import Button from '../common/button';
 
 export default async function Header() {
   const { id } = await getSession();
 
-  return <ClientHeader id={id} />;
+  return (
+    <header className="bg-background/80 sticky top-0 z-[var(--z-header)] mb-16 w-full border-b backdrop-blur-[5px] backdrop-saturate-[180%]">
+      <div className="mx-auto flex h-16 w-full max-w-(--breakpoint-xl) items-center justify-between px-4">
+        <Nav />
+
+        {id && (
+          <Form action={logoutAction}>
+            <Button className="h-8 rounded-md text-sm">Log out</Button>
+          </Form>
+        )}
+      </div>
+    </header>
+  );
 }
---
+++ src/components/layout/nav.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+
+const NAV_LINKS = [
+  { name: 'Posts', href: '/posts' },
+  { name: 'About', href: '/about' },
+];
+
+export default function Nav() {
+  const pathname = usePathname();
+
+  return (
+    <nav>
+      <ul className="flex items-center gap-6">
+        {NAV_LINKS.map(({ name, href }) => (
+          <li key={href}>
+            <Link
+              className={cn(
+                'text-secondary-foreground hover:text-foreground text-sm transition-colors duration-150 ease-in-out',
+                pathname.startsWith(href) && 'text-accent-foreground hover:text-accent-foreground',
+              )}
+              href={href}
+            >
+              {name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
