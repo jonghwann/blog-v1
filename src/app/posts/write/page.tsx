28bcdce0bee16982f7202ec9909ed9fdaa6dc082
jangjonghwan <wkdwhdghks97@gmail.com>
2025-06-07 16:09:54 +0900

    feat: EditorTags 컴포넌트 구현

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -2,6 +2,7 @@ import Form from 'next/form';
 
 import Input from '@/components/common/input';
 import Editor from '@/components/editor/editor';
+import EditorTags from '@/components/editor/editor-tags';
 
 export default function WritePage() {
   const handleSubmit = async (formData: FormData) => {
@@ -12,11 +13,11 @@ export default function WritePage() {
   };
 
   return (
-    <section className="mx-auto w-full max-w-(--breakpoint-md) px-4">
+    <section className="relative mx-auto w-full max-w-(--breakpoint-md) px-4">
       <Form action={handleSubmit}>
-        <Input className="border-none px-2 focus-visible:ring-0" name="title" placeholder="제목을 입력해주세요." />
+        <Input className="border-none px-0 focus-visible:ring-0" name="title" placeholder="제목을 입력해 주세요." />
         <Editor />
-        <button type="submit">Submit</button>
+        <EditorTags className="mb-5" />
       </Form>
     </section>
   );
