c4389cca6ce9401b85c7a3fa09050233d3676da3
jangjonghwan <wkdwhdghks97@gmail.com>
2025-06-10 23:01:49 +0900

    feat: 게시글 상세 페이지 ISR 적용

--- src/api/post/[id]/api.ts
+++ src/api/post/[id]/api.ts
@@ -1,5 +1,11 @@
 export async function getPost(id: string) {
-  const response = await fetch(`http://localhost:3000/api/posts/${id}`);
+  'use cache';
+  const response = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    next: {
+      revalidate: 31536000,
+    },
+  });
+
   const data = await response.json();
   return data;
 }
