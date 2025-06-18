c74332f7eec1cfc8b2cf2edf7dfa7b26c283402f
jonghwann <jonghwan0729@gmail.com>
2025-06-18 23:00:49 +0900

    feat: 게시글 상세 페이지 SSG 적용 및 정적 경로 생성 로직 추가

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,8 @@ import { useAuthStore } from '@/store/auth';
 
 import { logoutAction } from '@/app/logout/action';
 
+import useScrollVisibility from '@/hooks/use-scroll-visibility';
+
 import Nav from './nav';
 import Button from '../common/button';
 
@@ -15,6 +17,8 @@ export default function Header() {
   const isLogin = useAuthStore((state) => state.isLogin);
   const setLogout = useAuthStore((state) => state.setLogout);
 
+  const marginTop = useScrollVisibility(64);
+
   const handleLogout = async () => {
     await logoutAction();
     setLogout();
@@ -22,7 +26,10 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-background/80 fixed top-0 z-[var(--z-header)] w-full border-b backdrop-blur-[5px] backdrop-saturate-[180%]">
+    <header
+      className="bg-background/80 fixed top-0 z-[var(--z-header)] w-full border-b backdrop-blur-[5px] backdrop-saturate-[180%]"
+      style={{ marginTop }}
+    >
       <div className="mx-auto flex h-16 w-full max-w-(--breakpoint-xl) items-center justify-between px-4">
         <Nav />
 
