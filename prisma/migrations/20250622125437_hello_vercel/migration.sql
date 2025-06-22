f51997db467bf9628397ab63680b616e3ee42713
jonghwann <jonghwan0729@gmail.com>
2025-06-22 21:56:28 +0900

    chore: Neon(PostgreSQL)과 Vercel 환경 연동 설정

---
+++ prisma/migrations/20250622125437_hello_vercel/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "summary" TEXT NOT NULL,
+    "tags" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,26 +1,25 @@
-// import { findPostById, findPosts } from '@/lib/db/posts';
+import { findPostById, findPosts } from '@/lib/db/posts';
 
 import BackButton from '@/components/common/back-button';
-// import PostContent from '@/components/post/post-content';
-// import PostTableOfContents from '@/components/post/post-table-of-contents';
+import PostContent from '@/components/post/post-content';
+import PostTableOfContents from '@/components/post/post-table-of-contents';
 
 interface PostPageProps {
   params: Promise<{ id: string }>;
 }
 
 export default async function PostPage({ params }: PostPageProps) {
-  console.log(params);
-  // const { id } = await params;
+  const { id } = await params;
 
-  // const post = await findPostById(Number(id));
+  const post = await findPostById(Number(id));
 
   return (
     <section className="mx-auto w-full max-w-(--breakpoint-md) px-4">
       <BackButton />
 
       <div className="flex gap-16">
-        {/* <PostContent className="w-full xl:min-w-[736px]" html={post?.content ?? ''} /> */}
-        {/* <PostTableOfContents className="hidden xl:block" content={post?.content ?? ''} /> */}
+        <PostContent className="w-full xl:min-w-[736px]" html={post?.content ?? ''} />
+        <PostTableOfContents className="hidden xl:block" content={post?.content ?? ''} />
       </div>
     </section>
   );
@@ -28,7 +27,7 @@ export default async function PostPage({ params }: PostPageProps) {
 
 export const dynamicParams = false;
 
-// export async function generateStaticParams() {
-// const posts = await findPosts();
-// return posts.map((post) => ({ id: String(post.id) }));
-// }
+export async function generateStaticParams() {
+  const posts = await findPosts();
+  return posts.map((post) => ({ id: String(post.id) }));
+}
