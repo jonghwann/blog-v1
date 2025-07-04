cbb29d91105d8294a198f4473237048960699e53
jangjonghwan <wkdwhdghks97@gmail.com>
2025-07-04 20:05:16 +0900

    refactor: shadcn input 제거 및 공통 input 로직 개선

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,25 +1,19 @@
-import { InputHTMLAttributes } from 'react';
-
 import { cn } from '@/lib/utils';
 
-import { Input as InputUI } from '@/components/ui/input';
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.ComponentProps<'input'> {
   className?: string;
   errorClassName?: string;
-  name: string;
   errors?: string[];
 }
 
-export default function Input({ className, errorClassName, name, errors, ...rest }: InputProps) {
+export default function Input({ className, errorClassName, errors, ...rest }: InputProps) {
   return (
     <div className="flex flex-col gap-2">
-      <InputUI
+      <input
         className={cn(
-          'placeholder:text-secondary-foreground h-12 rounded-lg py-0 text-base! shadow-none autofill:shadow-[0_0_0_1000px_var(--background)_inset] focus-visible:ring-2',
+          'placeholder:text-secondary-foreground h-12 w-full min-w-0 rounded-lg border bg-transparent px-3 text-base transition-shadow outline-none autofill:shadow-[0_0_0_1000px_var(--background)_inset] focus-visible:ring-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
-        name={name}
         {...rest}
       />
 
