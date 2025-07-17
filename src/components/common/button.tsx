9a0a5e5c4716fc248d38d911f2deedd0497ef6db
jonghwann <jonghwan0729@gmail.com>
2025-07-17 21:53:37 +0900

    feat: 404 Not Found 페이지 추가

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,24 +1,33 @@
 import { type VariantProps } from 'class-variance-authority';
 import { Loader2 } from 'lucide-react';
+import { Fragment } from 'react';
 
 import { Button as UIButton, buttonVariants } from '@/components/ui/button';
 
 interface ButtonProps extends React.ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
   className?: string;
   isLoading?: boolean;
+  asChild?: boolean;
   children: React.ReactNode;
 }
 
-export default function Button({ className, variant = 'default', isLoading, children, ...props }: ButtonProps) {
+export default function Button({
+  className,
+  variant = 'default',
+  isLoading,
+  asChild = false,
+  children,
+  ...props
+}: ButtonProps) {
   return (
-    <UIButton className={`relative ${className}`} variant={variant} disabled={isLoading} {...props}>
-      <span className={isLoading ? 'invisible' : ''}>{children}</span>
+    <UIButton className={`relative ${className}`} variant={variant} disabled={isLoading} asChild={asChild} {...props}>
+      <Fragment>
+        <span className={isLoading ? 'invisible' : ''}>{children}</span>
 
-      {isLoading && (
-        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Loader2 className="h-[1em] w-[1em] animate-spin" />
-        </span>
-      )}
+        {isLoading && (
+          <Loader2 className="absolute top-1/2 left-1/2 h-[1em] w-[1em] -translate-x-1/2 -translate-y-1/2 animate-spin" />
+        )}
+      </Fragment>
     </UIButton>
   );
 }
