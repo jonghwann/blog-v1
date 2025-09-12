91db44268d269eed98f8a728398ec16a80f67d45
jonghwann <jonghwan0729@gmail.com>
2025-09-12 21:17:21 +0900

    style: 포스트 목록 스타일 수정

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -1,38 +1,26 @@
 import Link from 'next/link';
-
 import { formatDate } from '@/lib/utils';
+import TagList from '../common/tag-list';
 
 interface PostItemProps {
   id: number;
   title: string;
-  summary: string;
-  readingTime: number;
   createdAt: Date;
+  summary: string;
+  tags: string[];
 }
 
-export default function PostItem({ id, title, summary, readingTime, createdAt }: PostItemProps) {
+export default function PostItem({ id, title, createdAt, summary, tags }: PostItemProps) {
   return (
     <article>
-      <Link className='group' href={`/posts/${id}`}>
-        <h2 className='mb-6 font-bold text-[32px] leading-[1.3]'>{title}</h2>
-        <time className='mb-4 block text-[14.4px] leading-[1]'>{formatDate(createdAt)}</time>
-        <p className='mb-8 text-[15px] leading-[1.7]'>{summary}</p>
-
-        {/* <div className='flex flex-col gap-3'>
-          <div className='flex flex-col gap-2'>
-            <h2 className='line-clamp-1 font-semibold text-base text-foreground transition-colors duration-200 ease-in-out group-hover:text-accent-foreground sm:text-xl'>
-              {title}
-            </h2>
-            <p className='line-clamp-2 text-secondary-foreground text-xs sm:text-sm'>{summary}</p>
-          </div>
-
-          <div className='flex items-center gap-2 text-secondary-foreground text-xs'>
-            <time>{formatDate(createdAt)}</time>
-            <span>·</span>
-            <span>{readingTime} min read</span>
-          </div>
-        </div> */}
+      <Link href={`/posts/${id}`}>
+        <h2 className='mb-6 font-bold text-[32px] leading-[1.2]'>{title}</h2>
       </Link>
+
+      <time className='mb-4 block text-sm text-tertiary-foreground leading-[1]'>{formatDate(createdAt)}</time>
+      <p className='mb-8 text-[15px] leading-[1.7]'>{summary}</p>
+
+      <TagList tags={tags} />
     </article>
   );
 }
