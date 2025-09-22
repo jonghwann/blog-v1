6a1e274bd35d2c1271744dfb6b37512e883a79c1
jonghwann <jonghwan0729@gmail.com>
2025-09-22 16:52:03 +0900

    feat: PostNavigation 컴포넌트 인터랙션 개선

--- src/components/common/scroll-progress-bar.tsx
+++ src/components/common/scroll-progress-bar.tsx
@@ -1,13 +1,12 @@
 'use client';
-
 import useScrollProgress from '@/hooks/use-scroll-progress';
 
 export default function ScrollProgressBar() {
   const scrollPercentage = useScrollProgress();
 
   return (
-    <div className="z-header fixed top-0 left-0 z-[var(--z-scroll-progress-bar)] h-1 w-full backdrop-blur-[5px]">
-      <div className="bg-accent-foreground h-full" style={{ width: `${scrollPercentage}%` }} />
+    <div className='fixed top-0 left-0 z-[var(--z-scroll-progress-bar)] z-header h-1 w-full backdrop-blur-[5px]'>
+      <div className='h-full bg-accent-foreground' style={{ width: `${scrollPercentage}%` }} />
     </div>
   );
 }
--- src/components/post/post-table-of-contents.tsx
+++ src/components/post/post-table-of-contents.tsx
@@ -1,7 +1,5 @@
 'use client';
-
 import Link from 'next/link';
-
 import useActiveHeadings from '@/hooks/use-active-headings';
 import { createTableOfContents } from '@/lib/posts';
 import { cn } from '@/lib/utils';
@@ -20,30 +18,27 @@ interface PostTableOfContentsProps {
 
 export default function PostTableOfContents({ className, content }: PostTableOfContentsProps) {
   const tableOfContents = createTableOfContents(content);
-  const activeId = useActiveHeadings({ tableOfContents, options: { rootMargin: '-82px 0px 0px 0px', threshold: 1 } });
+  const activeId = useActiveHeadings(tableOfContents);
 
   return (
-    <aside className={cn('block w-[200px] shrink-0', className)}>
-      <div className='sticky top-[128px] text-xs'>
-        <h2 className='mb-3 font-medium text-sm'>On this page</h2>
-        <nav>
-          <ul className='flex flex-col gap-1.5'>
-            {tableOfContents.map(({ id, title, link, depth }) => (
-              <li key={title} className={depth ? 'ml-3' : ''}>
-                <Link
-                  className={cn(
-                    'transition-colors duration-200 ease-in-out hover:text-foreground',
-                    id + 1 === activeId ? 'font-semibold text-accent-foreground' : 'text-secondary-foreground',
-                  )}
-                  href={link}
-                >
-                  {title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+    <aside className={cn('block w-[240px] shrink-0', className)}>
+      <nav className='sticky top-[100px] font-medium text-sm'>
+        <ul className='flex flex-col gap-2'>
+          {tableOfContents.map(({ id, title, link, depth }) => (
+            <li key={title} className={depth ? 'ml-4' : ''}>
+              <Link
+                className={cn(
+                  'block transition-transform',
+                  id === (activeId ?? 0) ? '-translate-x-[10px] text-tertiary-foreground' : 'translate-x-0 text-ring-focus',
+                )}
+                href={link}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 }
