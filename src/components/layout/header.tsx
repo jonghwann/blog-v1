6a4130f53bd2cd8d9d70531b10ae95f13be3065e
jangjonghwan <wkdwhdghks97@gmail.com>
2025-09-22 14:43:57 +0900

    feat: PostNavigation 컴포넌트 인터페이스 개선

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       className='fixed top-0 z-[var(--z-header)] w-full bg-background/80 px-4 shadow-[var(--shadow)] backdrop-blur-[5px]'
       style={{ marginTop }}
     >
-      <div className='mx-0 flex h-14 items-center justify-between md:mx-16'>
+      <div className='mx-0 flex h-14 items-center justify-between min-[860px]:mx-16'>
         <Link href='/posts' className='font-source-code-pro text-sm tracking-[-1px] sm:text-2xl'>
           jonghwan.blog
         </Link>
