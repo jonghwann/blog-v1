d48098e004b8b72cda98c3b0a1aff4e9a1cbb866
jonghwann <jonghwan0729@gmail.com>
2025-09-29 10:49:56 +0900

    feat: 404 페이지 디자인 개선

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+import Button from '@/components/common/button';
+
 export default function NotFound() {
   return (
-    <section className="absolute inset-0 flex items-center justify-center">
-      <h1 className="mr-5 border-r pr-5 text-2xl">404</h1>
-      <p className="text-sm">This page could not be found.</p>
+    <section className='absolute inset-0 flex flex-col items-center justify-center'>
+      <h1 className='mb-4 font-bold text-2xl'>Not Found</h1>
+
+      <p className='mb-8 text-lg'>찾을 수 없는 페이지입니다.</p>
+
+      <Link href='/posts'>
+        <Button variant='secondary' className='h-10 w-[116px]'>
+          홈으로
+        </Button>
+      </Link>
     </section>
   );
 }
